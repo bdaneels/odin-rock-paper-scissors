de9536070fdb8429d12b9018a40553b71f361c86
bdaneels <118131373+bdaneels@users.noreply.github.com>
2022-12-14 16:51:53 +0100

    complete main function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let choicesArray = ["rock", "paper", "scissors"]
 
+
 function getComputerChoice (choices) {
     let index = Math.floor(Math.random() * choices.length);
     return choices[index]
@@ -11,3 +12,36 @@ let playerChoice = (prompt("What's your choice? Rock/Paper/Scissors:")).toLowerC
 
 console.log(computerChoice)
 console.log(playerChoice)
+
+function determineWinner(computerSelection, playerSelection) {
+
+    if (computerSelection === playerSelection) {
+        return `Both chose ${computerChoice}. It's a draw!`
+    }
+    
+    else if (computerSelection === "rock" && playerSelection === "paper") {
+        return "You Win! Paper beats Rock"
+    }
+
+    else if (computerSelection === "rock" && playerSelection === "scissors") {
+        return "You Lose! Rock beats Scissors"
+    }
+
+    else if (computerSelection === "paper" && playerSelection === "scissors") {
+        return "You Win! Scissors beat paper"
+    }
+
+    else if (computerSelection === "paper" && playerSelection === "rock") {
+        return "You Lose! Paper beats Rock"
+    }
+    
+    else if (computerSelection === "scissors" && playerSelection === "rock") {
+        return "You Win! Rock beats Scissors"
+    }
+   
+    else if (computerSelection === "scissors" && playerSelection === "paper") {
+        return "You Lose! Scissors beat paper"
+    }
+}
+
+console.log(determineWinner(computerChoice, playerChoice))
